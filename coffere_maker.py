5ba0422e51b23462619d6566c5d50c653cc67ae1
mbali phulwane <mbphulwjhb024@student.wethinkcode.co.za>
2025-03-21 20:07:05 +0200

    processing money

--- coffere_maker.py
+++ coffere_maker.py
@@ -38,4 +38,14 @@ def check_resources(order_ingredients):
             return False
     return True
 
+def process_notes():
+    try:
+        print("Please insert notes.")
+        note_ten = int(input("How many R10 notes?: "))
+        note_twenty = int(input("How many R20 notes?: "))
+        note_fifty = int(input("How many R50 notes?: "))
+        return note_ten * 10 + note_twenty * 20 + note_fifty * 50
+    except ValueError:
+        print("Invalid input. Please enter numeric values only.")
+        return 0
 
